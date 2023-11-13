a544ae7f209161ee1f5f2c957189516c338c4da7
Feysal <99170459+3Faysal@users.noreply.github.com>
2023-11-13 10:51:44 +0100

    k

--- Siege/Assets/CharacterMovement.cs
+++ Siege/Assets/CharacterMovement.cs
@@ -11,7 +11,8 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField]
     float rotateSpeed;
 
-    float allowedRot;
+    float allowedRotX;
+    float allowedRotY;
 
     [SerializeField]
     GameObject RotateObject;
@@ -21,11 +22,13 @@ public class CharacterMovement : MonoBehaviour
         hInput = Input.GetAxis("Horizontal");
         yInput = Input.GetAxis("Vertical");
 
-        transform.Rotate(new Vector3(0f, hInput * rotateSpeed * Time.deltaTime, 0f));
+        //transform.Rotate(new Vector3(0f, hInput * rotateSpeed * Time.deltaTime, 0f));
 
-        allowedRot = Mathf.Clamp(allowedRot + yInput, 0f, 65f);
-        
-        RotateObject.transform.rotation = Quaternion.Euler(allowedRot, gameObject.transform.rotation.y, gameObject.transform.rotation.z);
+        allowedRotX = Mathf.Clamp(allowedRotX + yInput, 0f, 65f);
+        allowedRotY = Mathf.Clamp(allowedRotY + hInput, Mathf.NegativeInfinity, Mathf.Infinity);
+
+        RotateObject.transform.rotation = Quaternion.Euler(allowedRotX, allowedRotY, 0f);
+        transform.rotation = Quaternion.Euler(0f, allowedRotY, 0f);
         //RotateObject.transform.Rotate(new Vector3(yInput * rotateSpeed * Time.deltaTime, 0f, 0f), Space.Self);
 
     }
